47c1f6f95543d057c444d16178ab348112b978c0
Christie <christie0610g@gmail.com>
2022-10-24 18:36:34 -0700

    fix conflicts

--- sales/api/sales_project/settings.py
+++ sales/api/sales_project/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
